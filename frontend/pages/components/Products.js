b9f4ba1fbf42888e259e639b90bc0c877ac75b79
piusprince <45078705+piusprince@users.noreply.github.com>
2022-07-28 19:58:11 +0000

    Update Products.js

--- frontend/pages/components/Products.js
+++ frontend/pages/components/Products.js
@@ -1,13 +1,16 @@
 import { ProductWrapper } from "../../styles/ProductStyle";
+import Link from "next/link";
 
 export default function Products({ product }) {
   // destructuring product object
-  const { title, price, description, image } = product.attributes;
+  const { title, price, slug, image } = product.attributes;
   return (
     <ProductWrapper>
-      <div>
-        <img src={image.data.attributes.formats.small.url} alt="" />
-      </div>
+      <Link href={`/product/${slug}`}>
+        <div>
+          <img src={image.data.attributes.formats.small.url} alt="" />
+        </div>
+      </Link>
       <h1>{title}</h1>
       <p>{price}</p>
     </ProductWrapper>
