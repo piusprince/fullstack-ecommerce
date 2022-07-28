ca85e57ef94de63cabf45071b6825f1b2247d809
piusprince <45078705+piusprince@users.noreply.github.com>
2022-07-28 14:29:55 +0000

    Update product page
    -Added routing to each individual page
    -Connected backend to frontend by consuming apis
    -Styled the product page

--- frontend/pages/product/[slug].js
+++ frontend/pages/product/[slug].js
@@ -1,25 +1,53 @@
 import { useQuery } from "urql";
 import { GET_PRODUCT_QUERY } from "../../lib/query";
+import { useRouter } from "next/router";
+import {
+  DetailsStyle,
+  ProductInfo,
+  Quantity,
+  Buy,
+} from "../../styles/ProductDetails";
 
 export default function ProductData() {
+  // fetch product url
+  const { query } = useRouter();
+  console.log(query);
+
   // fetch product data with graphql query
-  const [results] = useQuery({ query: GET_PRODUCT_QUERY });
+  const [results] = useQuery({
+    query: GET_PRODUCT_QUERY,
+    variables: { slug: query.slug },
+  });
   console.log(results);
+  const { data, fetching, error } = results;
+
+  if (fetching) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Oops could not fetch data: {error.message}</p>;
+  }
+  console.log(data);
+
+  // get product data
+
+  const { description, title, image } = data.products.data[0].attributes;
 
   return (
-    <div>
-      <img src="" alt="" />
-      <div>
-        <h3>title</h3>
-        <p>description</p>
-      </div>
-      <div>
-        <span>quantity</span>
-        <button>+</button>
-        <p>0</p>
-        <button>-</button>
-      </div>
-      <button>Add to cart</button>
-    </div>
+    <DetailsStyle>
+      <img src={image.data.attributes.formats.medium.url} alt={title} />
+      <ProductInfo>
+        <h3>{title}</h3>
+        <p>{description}</p>
+
+        <Quantity>
+          <span>quantity</span>
+          <button>+</button>
+          <p>0</p>
+          <button>-</button>
+        </Quantity>
+        <Buy>Add to cart</Buy>
+      </ProductInfo>
+    </DetailsStyle>
   );
 }
