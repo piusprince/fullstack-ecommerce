7866072f0a747e1ebd27d29b6c837883bfba6532
piusprince <45078705+piusprince@users.noreply.github.com>
2022-07-29 05:38:08 +0000

    Update Nav.js

--- frontend/pages/components/Nav.js
+++ frontend/pages/components/Nav.js
@@ -1,17 +1,22 @@
 import Link from "next/link";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { NavMenu, NavWrapper } from "../../styles/NavStyles";
+import Cart from "./Cart";
+import { useStateContext } from "../../lib/context";
 
 export default function Nav() {
+  const { showCart, setShowCart } = useStateContext();
   return (
     <NavWrapper>
       <Link href="/">Drip Hub</Link>
       <NavMenu>
-        <div>
+        <div onClick={() => setShowCart(true)}>
           <AiOutlineShoppingCart fontSize={25} />
           <h3>Cart</h3>
         </div>
       </NavMenu>
+      {showCart && <Cart />}
+      {/* <Cart /> */}
     </NavWrapper>
   );
 }
