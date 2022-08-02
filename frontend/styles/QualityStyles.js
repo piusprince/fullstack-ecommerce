07bd384af4670f21c0815a4dcd74e68166406c0c
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 20:10:59 +0000

    Create QualityStyles.js

---
+++ frontend/styles/QualityStyles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const QualityWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  /* margin: 0 auto; */
+  width: 100%;
+  gap: 5rem;
+`;
+
+export const TextWrapper = styled.div`
+  p {
+    margin-top: 2rem;
+  }
+`;
+
+export const Video = styled.div`
+  /* width: 50%;
+  height: 50%; */
+  margin-top: 50px;
+`;
