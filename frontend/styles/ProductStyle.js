3be0a2bedf3c84f7272ad9f331692ab762614540
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 20:10:57 +0000

    Update ProductStyle.js

--- frontend/styles/ProductStyle.js
+++ frontend/styles/ProductStyle.js
@@ -16,3 +16,21 @@ export const ProductWrapper = styled.div`
     font-size: 1.5rem;
   }
 `;
+
+export const ProductInfo = styled.div`
+  h2 {
+    background: var(--primary);
+    color: white;
+    /* border-radius: 0 0.5rem 0 0; */
+    padding: 0.5rem;
+    width: 85%;
+  }
+
+  p {
+    background: var(--secondary);
+    color: white;
+    padding: 0.5rem;
+    width: 30%;
+    margin-top: 0.2rem;
+  }
+`;
