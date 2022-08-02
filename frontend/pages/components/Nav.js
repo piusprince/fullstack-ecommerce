2610d39b5c6d66c33dd38d4d9858b31b6741745c
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 06:08:02 +0000

    Update Nav.js
    - Added feature of showing total items in cart to navbar

--- frontend/pages/components/Nav.js
+++ frontend/pages/components/Nav.js
@@ -5,12 +5,13 @@ import Cart from "./Cart";
 import { useStateContext } from "../../lib/context";
 
 export default function Nav() {
-  const { showCart, setShowCart } = useStateContext();
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <NavWrapper>
       <Link href="/">Drip Hub</Link>
       <NavMenu>
         <div onClick={() => setShowCart(true)}>
+          {totalQuantities > 0 && <span>{totalQuantities}</span>}
           <AiOutlineShoppingCart fontSize={25} />
           <h3>Cart</h3>
         </div>
