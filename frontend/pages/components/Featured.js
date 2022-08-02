7dcaf6c2b57b7b47860d5665bdce4880097b4412
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 20:08:06 +0000

    Create Featured.js

---
+++ frontend/pages/components/Featured.js
@@ -0,0 +1,9 @@
+import { FeaturedWrapper } from "../../styles/FeaturedStyles";
+
+export default function Featured() {
+  return (
+    <FeaturedWrapper>
+      <h1>Featured items on sale</h1>
+    </FeaturedWrapper>
+  );
+}
