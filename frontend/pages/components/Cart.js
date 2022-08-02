4c80c13ff5f4c65f0e0acbab03bdbfc667756309
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 09:44:18 +0000

    update: Add animations to components

--- frontend/pages/components/Cart.js
+++ frontend/pages/components/Cart.js
@@ -2,6 +2,7 @@ import { useStateContext } from "../../lib/context";
 import {
   CartStyle,
   CartWrapper,
+  Cards,
   Card,
   CardInfo,
   EmptyCart,
@@ -14,48 +15,87 @@ import {
   AiFillPlusCircle,
 } from "react-icons/ai";
 
+// Variants with framer motion
+const cardVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+const cardsVariant = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
 export default function Cart() {
   const { cartItems, setShowCart, addToCart, removeFromCart, totalPrice } =
     useStateContext();
   return (
-    <CartWrapper onClick={() => setShowCart(false)}>
-      <CartStyle onClick={(e) => e.stopPropagation()}>
-        {cartItems.length < 1 ? (
-          <EmptyCart>
-            <h3>Your cart is empty</h3>
-            <p>You have no items in your cart.</p>
-            <AiOutlineShoppingCart />
-          </EmptyCart>
-        ) : (
-          cartItems.map((item) => (
-            <Card key={item.id}>
-              <img
-                src={item.image.data.attributes.formats.thumbnail.url}
-                alt={item.title}
-              />
+    <CartWrapper
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      onClick={() => setShowCart(false)}
+    >
+      <CartStyle
+        initial={{ x: "50%" }}
+        animate={{ x: "0%" }}
+        transition={{ type: "tween", duration: 0.5 }}
+        exit={{ x: "50%" }}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <Cards
+          variants={cardsVariant}
+          initial="hidden"
+          animate="visible"
+          layout
+        >
+          {cartItems.length < 1 ? (
+            <EmptyCart
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ type: "tween", duration: 0.3 }}
+            >
+              <h3>Your cart is empty</h3>
+              <p>You have no items in your cart.</p>
+              <AiOutlineShoppingCart />
+            </EmptyCart>
+          ) : (
+            cartItems.map((item) => (
+              <Card variants={cardVariant} layout key={item.id}>
+                <img
+                  src={item.image.data.attributes.formats.thumbnail.url}
+                  alt={item.title}
+                />
 
-              <CardInfo>
-                <h3>{item.title}</h3>
-                <p>
-                  <strong>Price: GHC{item.price}</strong>
-                </p>
-                <QuantityWrapper>
-                  <span>
-                    <strong>Quantity: </strong>
-                  </span>
-                  <button onClick={() => removeFromCart(item)}>
-                    <AiFillMinusCircle />
-                  </button>
-                  <p>{item.quantity}</p>
-                  <button onClick={() => addToCart(item, 1)}>
-                    <AiFillPlusCircle />
-                  </button>
-                </QuantityWrapper>
-              </CardInfo>
-            </Card>
-          ))
-        )}
-        {cartItems.length >= 0 && (
+                <CardInfo>
+                  <h3>{item.title}</h3>
+                  <p>
+                    <strong>Price: GHC{item.price}</strong>
+                  </p>
+                  <QuantityWrapper>
+                    <span>
+                      <strong>Quantity: </strong>
+                    </span>
+                    <button onClick={() => removeFromCart(item)}>
+                      <AiFillMinusCircle />
+                    </button>
+                    <p>{item.quantity}</p>
+                    <button onClick={() => addToCart(item, 1)}>
+                      <AiFillPlusCircle />
+                    </button>
+                  </QuantityWrapper>
+                </CardInfo>
+              </Card>
+            ))
+          )}
+        </Cards>
+        {cartItems.length >= 1 && (
           <div>
             <p>
               <strong>Total: GHC{totalPrice}</strong>
