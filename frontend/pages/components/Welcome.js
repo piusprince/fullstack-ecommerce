cb8272cdb77eac6ba7d1389065619a50487f77dd
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 06:08:12 +0000

    Create Welcome.js

---
+++ frontend/pages/components/Welcome.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Welcome() {
+  return (
+    <div>
+      <div>
+        <h1 style={{ marginBottom: 20 }}>The Store for sneakers</h1>
+        {/* <Image
+          src="https://images.unsplash.com/photo-1631177218192-5f52cbdbab4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          alt="Drip Hub Background"
+          width={1200}
+          height={300}
+          objectFit="cover"
+        /> */}
+      </div>
+    </div>
+  );
+}
