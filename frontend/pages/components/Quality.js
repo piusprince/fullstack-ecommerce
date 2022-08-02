3d6c8ec52509515f5a5d0fb825d7a0c5ca99e2bb
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 20:08:27 +0000

    Create Quality.js

---
+++ frontend/pages/components/Quality.js
@@ -0,0 +1,36 @@
+import YouTube from "react-youtube";
+import { QualityWrapper, TextWrapper, Video } from "../../styles/QualityStyles";
+
+export default function Quality() {
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <QualityWrapper>
+      <TextWrapper>
+        <h1>Why shop with us</h1>
+        <p>
+          We are a family-owned and operated business that specializes in
+          quality products. We are committed to providing the highest quality
+          products to our customers. We also endeavour to teach our customers
+          how the various farm tools are expected to be used on farms
+        </p>
+      </TextWrapper>
+      <Video>
+        <YouTube
+          videoId="8Y7RX1CxK8w"
+          opts={opts}
+          onReady={() => {
+            console.log("Ready");
+          }}
+        />
+      </Video>
+    </QualityWrapper>
+  );
+}
