0c5ed8133dfdaf87e985aaeee282f2438dea8ba6
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 20:07:26 +0000

    update: Add footer component

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { Provider, createClient } from "urql";
 import Nav from "./components/Nav";
 import { StateContext } from "../lib/context";
+import Footer from "./components/Footer";
 
 const client = createClient({
   url: process.env.NEXT_PUBLIC_BACKEND_API,
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }) {
       <Provider value={client}>
         <Nav />
         <Component {...pageProps} />
+        <Footer />
       </Provider>
     </StateContext>
   );
