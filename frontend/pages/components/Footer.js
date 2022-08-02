bd46d0ec2fbb0c1072b0d6b019e8ef6c08ca9521
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 20:08:11 +0000

    Create Footer.js

---
+++ frontend/pages/components/Footer.js
@@ -0,0 +1,22 @@
+import { FooterWrapper } from "../../styles/FooterStyles";
+
+export default function Footer() {
+  return (
+    <FooterWrapper>
+      <p>&copy; 2020 FarmHub</p>
+      <div>
+        <h2>Category 1</h2>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+      </div>
+      <div>
+        <h2>Category 2</h2>
+        <p>Product</p>
+        <p>Product</p>
+      </div>
+    </FooterWrapper>
+  );
+}
