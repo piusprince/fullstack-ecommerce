f3eb67cc06deecbc0dc0d22553eb7db683422943
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-02 06:04:42 +0000

    update: Add new states
    -Added states for increasing total quantities to be shown on the cart icon
    -Added state to check the price of items

--- frontend/lib/context.js
+++ frontend/lib/context.js
@@ -6,6 +6,8 @@ export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [quantity, setQuantity] = useState(1);
+  const [totalQuantities, setTotalQuantities] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   // increase quantity of products
   const increaseQuantity = () => {
@@ -19,6 +21,12 @@ export const StateContext = ({ children }) => {
 
   // add product to cart
   const addToCart = (product, quantity) => {
+    // Increase product quantity
+    setTotalQuantities((prevTotal) => prevTotal + quantity);
+
+    // Increase product price
+    setTotalPrice((prevTotal) => prevTotal + product.price * quantity);
+
     // check if product already exists in cart
     const productExists = cartItems.find((item) => item.slug === product.slug);
     if (productExists) {
@@ -38,10 +46,16 @@ export const StateContext = ({ children }) => {
 
   // remove product from cart
   const removeFromCart = (product) => {
+    // decrease product quantity
+    setTotalQuantities((prevTotal) => prevTotal - 1);
+
+    // decrease product price
+    setTotalPrice((prevTotal) => prevTotal - product.price);
+
     // check if product is in the cart
     const productExists = cartItems.find((item) => item.slug === product.slug);
     if (productExists.quantity === 1) {
-      // remove product from cart
+      // remove product from cart if quantity is less than 1
       setCartItems(cartItems.filter((item) => item.slug !== product.slug));
     } else {
       setCartItems(
@@ -66,6 +80,8 @@ export const StateContext = ({ children }) => {
         setCartItems,
         addToCart,
         removeFromCart,
+        totalQuantities,
+        totalPrice,
       }}
     >
       {children}
