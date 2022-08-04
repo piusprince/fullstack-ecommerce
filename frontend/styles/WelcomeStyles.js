130ae48e7b7d9bd6d6abc436d2260f8a4923deb1
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-04 15:46:12 +0000

    update: Add responsiveness to component

--- frontend/styles/WelcomeStyles.js
+++ frontend/styles/WelcomeStyles.js
@@ -1,43 +1,63 @@
 import styled from "styled-components";
 
-export const ImageWrapper = styled.div`
-  position: relative;
+export const WelcomeWrapper = styled.image`
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2rem;
+  background-image: linear-gradient(
+      to right bottom,
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
+    ),
+    url("https://images.unsplash.com/photo-1600150806193-cf869bcfee05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80");
+  background-size: cover;
+  object-fit: cover;
+  background-repeat: no-repeat;
+  background-position: center;
   width: 100%;
-  height: 100%;
-  object-fit: contain;
-  margin-bottom: 5rem;
-`;
+  height: 100vh;
 
-export const TextWrapper = styled.div`
-  position: absolute;
-  top: 10%;
-  z-index: 100;
-  padding: 5rem;
+  div {
+    /* background-image: linear-gradient(
+      to right bottom,
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
+    ); */
 
-  h1 {
-    color: white;
-    font-size: 3.2rem;
-    font-weight: 700;
-    width: 75%;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    width: 100%;
+    height: 100%;
+    padding-top: 35%;
+    padding-left: 8%;
+
+    h1 {
+      font-size: 3rem;
+      font-weight: 700;
+      color: #fff;
+    }
+
+    p {
+      font-weight: 300;
+      color: #fff;
+    }
+
+    button {
+      align-self: flex-start;
+      font-size: 1.5rem;
+      font-weight: 700;
+      background: #ffc107;
+      width: ;
+      border: none;
+      padding: 1rem 2rem;
+      border-radius: 10px;
+      cursor: pointer;
+    }
   }
 
-  p {
-    font-size: 1.6rem;
-    font-weight: 300;
-    margin-top: 1rem;
-    color: white;
-    width: 50%;
-  }
-
-  button {
-    background: #ffc107;
-    border: none;
-    border-radius: 5px;
-    padding: 1rem;
-    font-size: 1.6rem;
-    font-weight: 700;
-    color: black;
-    cursor: pointer;
-    margin-top: 2rem;
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
   }
 `;
