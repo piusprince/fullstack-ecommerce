03d08c04c8dc9e9f21f96a0fe0fd04c67768290e
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-30 03:06:01 +0000

    update: Add profile image to navbar

--- frontend/pages/components/Nav.js
+++ frontend/pages/components/Nav.js
@@ -3,16 +3,20 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { NavMenu, NavWrapper } from "../../styles/NavStyles";
 import Cart from "./Cart";
 import { useStateContext } from "../../lib/context";
+import User from "./User";
+import { UseUser } from "@auth0/nextjs-auth0";
 
 const { AnimatePresence, motion } = require("framer-motion");
 
 export default function Nav() {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
+  const { user, error, isLoading } = useUser();
   return (
     <NavWrapper>
       <Link href="/">FarmHub</Link>
 
       <NavMenu>
+        <User />
         <div onClick={() => setShowCart(true)}>
           {totalQuantities > 0 && (
             <motion.span initial={{ scale: 0 }} animate={{ scale: 1 }}>
