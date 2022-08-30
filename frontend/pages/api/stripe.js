ab79fd5ad7da7b4a6c65e8e1a8dc3b30505c9b4c
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-30 03:05:27 +0000

    update: Add features to stripe
    -Added more countries to ship to
    -Fixed bug of not being able to checkout
    -Added routing to success page when checkout is completed

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -5,6 +5,8 @@ const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
 export default async function handler(req, res) {
   if (req.method === "POST") {
     console.log(req.body);
+    const { cartItems } = req.body;
+
     try {
       // Create checkout session
       const session = await stripe.checkout.sessions.create({
@@ -12,9 +14,19 @@ export default async function handler(req, res) {
         mode: "payment",
         payment_method_types: ["card"],
         shipping_address_collection: {
-          allowed_countries: ["US"],
+          allowed_countries: ["US", "GH", "KE", "NG", "CA", "GB", "AU", "NZ"],
         },
-        line_items: req.body.map((item) => {
+        allow_promotion_codes: true,
+        shipping_options: [
+          {
+            shipping_rate: "shr_1LcIX2ExMvZw8jQeP79NH5yV",
+          },
+          {
+            shipping_rate: "shr_1LcIn0ExMvZw8jQekmks0pta",
+          },
+        ],
+
+        line_items: cartItems.map((item) => {
           return {
             price_data: {
               currency: "usd",
@@ -24,12 +36,23 @@ export default async function handler(req, res) {
               },
               unit_amount: item.price * 100,
             },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
           };
         }),
+
+        // redirect to success page or cancel page
+        success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/canceled`,
       });
       res.status(200).json(session);
     } catch (error) {
-      res.status(error.statusCode || 500).json(error.message);
+      console.log("this shit broke", error);
+      res.status(500).json({ error });
+      // res.status(error.statusCode || 500).json(error.message);
     }
   }
 }
