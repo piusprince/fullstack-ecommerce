62b9e154fdad3c73400a00b084c92182daf8a582
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-30 03:08:53 +0000

    fix: Remove video autoplay

--- frontend/pages/components/Quality.js
+++ frontend/pages/components/Quality.js
@@ -7,7 +7,7 @@ export default function Quality() {
     width: "100%",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
+      autoplay: 0,
     },
   };
 
