100b575023dd31c60b06785f84aa09507b1c645a
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-30 05:01:49 +0000

    Update User.js

--- frontend/pages/components/User.js
+++ frontend/pages/components/User.js
@@ -1,15 +1,23 @@
 import { useRouter } from "next/router";
 import styled from "styled-components";
 import { FaUserCircle } from "react-icons/fa";
+import { useUser } from "@auth0/nextjs-auth0";
 
 export default function User() {
   const route = useRouter();
-
+  const { user } = useUser();
+  if (!user)
+    return (
+      <div onClick={() => route.push("/api/auth/login")}>
+        <FaUserCircle />
+        <h3>Profile</h3>
+      </div>
+    );
   return (
-    <div onClick={() => route.push("/api/auth/login")}>
-      <FaUserCircle />
-      <h3>Profile</h3>
-    </div>
+    <Profile onClick={() => route.push("/profile")}>
+      <img src={user.picture} alt={user.name}></img>
+      <h3>{user.name}</h3>
+    </Profile>
   );
 }
 
