d85af9df92901b218642741777f74a28c5ea0cc1
piusprince <45078705+piusprince@users.noreply.github.com>
2022-08-30 05:00:24 +0000

    feat: Redirect cancel on ordering to home

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/canceled",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
