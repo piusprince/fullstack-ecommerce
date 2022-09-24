e6d4a9ea2cb203d121f191349f62765f679cfd30
piusprince <45078705+piusprince@users.noreply.github.com>
2022-09-24 22:29:03 +0000

    Update plugins.js

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: "cloudinary",
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME", "piusprince"),
-        api_key: env("CLOUDINARY_KEY", "934431228173379"),
-        api_secret: env("CLOUDINARY_SECRET", "2YqTVYohtpJ7ERJgqDejfoZtx0w"),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
